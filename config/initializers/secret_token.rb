f10428d22eeb37fee1e9b9d6a34a92f3bcb7a85c
ecavazos <ejcavazos@gmail.com>
2012-03-16 20:35:38 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperZombies::Application.config.secret_token = 'e373d9e5952447c2be2653ae6921fc3cdbbd3e1b7878ae8978ea717767fe109f08558a058bc027d0b37d79c76fd924cf6d343d16aa2f5ecb20811d2d1131b64d'
